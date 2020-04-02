c7d2c6877ee8d13acf267a00581919d94a2d52dc
Nick Williams <ntwilliams.personal@gmail.com>
2020-04-02 18:57:20 -0400

    removing unnecessary dependencies

--- tests/testthat/test-time_varying_treatment.R
+++ tests/testthat/test-time_varying_treatment.R
@@ -32,7 +32,7 @@ ipw <-
 tmle <-
   lmtp_tmle(df, a, "Y", nodes, k = 0, shift = function(x) x + 0.5,
             outcome_type = "binomial",
-            learner_stack_Q = sl3::make_learner(sl3::Lrnr_glm_fast),
+            learner_stack_Q = sl3::make_learner(sl3::Lrnr_glm),
             learner_stack_g = sl3::make_learner_stack(sl3::Lrnr_glm))
 
 sdr <-
