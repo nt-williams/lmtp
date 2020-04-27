efd148a8dd281496d2f7ec6e4c7c756963a6217a
Nick Williams <ntwilliams.personal@gmail.com>
2020-04-27 16:56:44 -0400

    folds now created in Meta

--- R/cross-fit.R
+++ R/cross-fit.R
@@ -1,13 +1,15 @@
 
 setup_cv <- function(data, V = 10) {
-  out <- list()
-  fold_index <- origami::make_folds(data, V = V)
+  out <- origami::make_folds(data, V = V)
+  return(out)
+}
 
+get_folded_data <- function(data, folds) {
+  out <- list()
   for (i in 1:length(fold_index)) {
     out[[i]] <- list()
     out[[i]][["train"]] <- data[fold_index[[i]]$training_set, ]
     out[[i]][["valid"]] <- data[fold_index[[i]]$validation_set, ]
   }
-
   return(out)
 }
