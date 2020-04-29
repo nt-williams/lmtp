23e46f444a5c614c46faad8e772debd95e308f70
Nick Williams <ntwilliams.personal@gmail.com>
2020-04-29 15:32:40 -0400

    back to passing basic checks

--- R/lmtp-package.R
+++ R/lmtp-package.R
@@ -1,4 +1,4 @@
-#' @importFrom stats as.formula coef glm plogis predict qlogis qnorm pnorm sd
+#' @importFrom stats as.formula coef glm plogis predict qlogis qnorm pnorm sd quantile
 #' @keywords internal
 "_PACKAGE"
 
