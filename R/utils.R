9c1e6782a6c46cfd5c08de2423869e81a69f4332
Nick Williams <ntwilliams.personal@gmail.com>
2020-04-30 10:53:39 -0400

    future result extract update

--- R/utils.R
+++ R/utils.R
@@ -102,3 +102,7 @@ recombine_ipw <- function(r) {
 recombine_dens_ratio <- function(r) {
   Reduce(rbind, lapply(r, function(x) x[["valid"]]))
 }
+
+get_future_result <- function(out) {
+  return(lapply(lapply(out, function(x) x$result), function(x) x$value))
+}
