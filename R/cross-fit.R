8f681014b03f75ad7f55143664dd3c20b1e1b86d
Nick Williams <ntwilliams.personal@gmail.com>
2020-05-06 12:57:17 -0400

    addressing bug caused by future ignoring global options

--- R/cross-fit.R
+++ R/cross-fit.R
@@ -16,8 +16,10 @@ get_folded_data <- function(data, folds) {
 
 cf_cens <- function(data, folded, V, C, outcome, tau, node_list, learners) {
   out <- list()
+  fopts <- options("lmtp.bound")
   for (i in 1:V) {
     out[[i]] <- future::future({
+      options(fopts)
       estimate_c(data, folded[[i]]$train, folded[[i]]$valid,
                  C, outcome, tau, node_list, learners)
       }, packages = "lmtp")
@@ -28,10 +30,11 @@ cf_cens <- function(data, folded, V, C, outcome, tau, node_list, learners) {
 
 cf_r <- function(data, shift, V, trt, cens, C,
                  tau, node_list, learners, pb) {
-
+  fopts <- options("lmtp.bound")
   out <- list()
   for (i in 1:V) {
     out[[i]] <- future::future({
+      options(fopts)
       estimate_r(data[[i]]$train, data[[i]]$valid, trt,
                  cens, C[[i]], shift, tau, node_list, learners, pb)
     }, packages = "lmtp")
@@ -42,10 +45,11 @@ cf_r <- function(data, shift, V, trt, cens, C,
 
 cf_sub <- function(data, shifted, V, outcome, node_list, C, tau,
                    outcome_type, learners, m, pb) {
-
+  fopts <- options("lmtp.bound")
   out <- list()
   for (i in 1:V) {
     out[[i]] <- future::future({
+      options(fopts)
       estimate_sub(data[[i]]$train, shifted[[i]]$train, shifted[[i]]$valid,
                    outcome, node_list, C, tau, outcome_type,
                    learners, m[[i]]$valid, pb)
@@ -59,9 +63,11 @@ cf_sub <- function(data, shifted, V, outcome, node_list, C, tau,
 cf_tmle <- function(data, shifted, V, outcome, node_list, C, tau,
                     outcome_type, m_natural, m_shifted, r, learners, pb) {
 
+  fopts <- options("lmtp.bound")
   m <- list()
   for (i in 1:V) {
     m[[i]] <- future::future({
+      options(fopts)
       estimate_tmle(data[[i]]$train, shifted[[i]]$train, data[[i]]$valid,
                     shifted[[i]]$valid, outcome, node_list, C, tau,
                     outcome_type, m_natural[[i]], m_shifted[[i]], r[[i]],
@@ -76,9 +82,12 @@ cf_tmle <- function(data, shifted, V, outcome, node_list, C, tau,
 
 cf_sdr <- function(data, shifted, V, outcome, node_list, C, tau,
                    outcome_type, m_natural, m_shifted, r, learners, pb) {
+
+  fopts <- options("lmtp.bound")
   m <- list()
   for (i in 1:V) {
     m[[i]] <- future::future({
+      options(fopts)
       estimate_sdr(data[[i]]$train, shifted[[i]]$train,data[[i]]$valid,
                    shifted[[i]]$valid,outcome, node_list, C, tau, tau,
                    outcome_type, learners,m_natural[[i]], m_shifted[[i]],
