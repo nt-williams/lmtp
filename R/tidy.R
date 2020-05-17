b08b00a37d932b07a6861ade10bdce7eafa36bcd
Nick Williams <ntwilliams.personal@gmail.com>
2020-05-17 13:02:37 -0400

    documentation updates

--- R/tidy.R
+++ R/tidy.R
@@ -9,21 +9,21 @@ generics::tidy
 #' [lmtp::lmtp_sub()], or [lmtp::lmtp_ipw()].
 #' @param ... Unused, included for generic consistency only.
 #'
-#' @examples # TODO
+#' @examples
+#' a <- c("A1", "A2")
+#' nodes <- list(c("L1"), c("L2"))
+#' cens <- c("C1", "C2")
+#' y <- "Y"
+#' fit <- lmtp_tmle(sim_cens, a, y, nodes, cens = cens, shift = NULL, folds = 2)
+#' tidy(fit)
 #'
 #' @export
 tidy.lmtp <- function(x, ...) {
-  if (requireNamespace("tibble", quietly = TRUE)) {
-    tibble::tibble(estimator = x$estimator,
-                   estimate = x$theta,
-                   std.error = x$standard_error,
-                   conf.low = x$low,
-                   conf.high = x$high)
-  } else {
-    as.data.frame(estimator = x$estimator,
-                  estimate = x$theta,
-                  std.error = x$standard_error,
-                  conf.low = x$low,
-                  conf.high = x$high)
-  }
+  out <- data.frame(estimator = x$estimator,
+                    estimate = x$theta,
+                    std.error = x$standard_error,
+                    conf.low = x$low,
+                    conf.high = x$high)
+  class(out) <- c("tbl_df", "tbl", "data.frame")
+  return(out)
 }
