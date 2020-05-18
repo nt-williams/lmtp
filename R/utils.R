420f24e67d335826b29c8507d6d705e4166b4005
Nick Williams <ntwilliams.personal@gmail.com>
2020-05-18 17:34:40 -0400

    documentation updates

--- R/utils.R
+++ R/utils.R
@@ -178,18 +178,24 @@ final_outcome <- function(outcomes) {
 
 #' Time To Event Last Outcome Carried Forward
 #'
+#' A helper function to prepare survival data for use with LMTP estimators
+#' by imputing outcome nodes using last outcome carried forward when an observation
+#' experiences the event before the end-of-follow-up.
+#'
 #' @param data The dataset to modify.
 #' @param outcomes A vector of outcome nodes ordered by time.
 #'
 #' @return A modified dataset with future outcome nodes set to 1 if an observation
 #'  experienced an event at any previous time point.
 #' @export
+#' @examples
+#' event_locf(sim_point_surv, c(paste0("N1.", 0:6)))
 event_locf <- function(data, outcomes) {
   tau <- length(outcomes)
   for (i in outcomes[1:(tau - 1)]) {
     modify <- setdiff(outcomes[match(i, outcomes):tau], i) # find all outcomes at later time
     for (j in 1:nrow(data)) {
-      if (data[j, i] == 1) data[j, modify] <- 1 # if the event is observed at the current time, set all later events to 1
+      if (data[j, i] == 1 & !is.na(data[j, i])) data[j, modify] <- 1 # if the event is observed at the current time, set all later events to 1
     }
   }
   return(data)
