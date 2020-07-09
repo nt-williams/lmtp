c547ccd25f70ee0f8dfa072e27db95252114bedd
Nick Williams <ntwilliams.personal@gmail.com>
2020-07-09 14:10:45 -0400

    removing unnecessary code

--- R/sl.R
+++ R/sl.R
@@ -3,19 +3,17 @@
 initiate_ensemble <- function(outcome_type, learners = NULL) {
 
   if (getOption("lmtp.engine") == "sl3") {
-    # specifying learners
-    sl3_profile <- learner_defaults(outcome_type = outcome_type, learners = learners)
 
     # building learner stack
     out <- sl3::make_learner(sl3::Lrnr_sl,
-                             learners = sl3_profile$learners,
-                             metalearner = sl3_profile$metalearner,
+                             learners = learners,
+                             metalearner = sl3::make_learner("Lrnr_nnls", convex = TRUE),
                              keep_extra = FALSE)
 
     # returns
     return(out)
   } else {
-    return(NULL) # NEED TO THINK ABOUT WHAT THIS SHOULD RETURN
+    return(NULL)
   }
 
 }
@@ -46,28 +44,6 @@ initiate_sl3_task <- function(data, Y, X, outcome_type, id = NULL, drop = FALSE)
 
 }
 
-# defines default meta learner and candidates if none specified
-learner_defaults <- function(outcome_type, learners) {
-
-  # setting meta learner
-  if (outcome_type %in% c("binomial", "quasibinomial", "continuous")) {
-    meta <- sl3::make_learner("Lrnr_nnls",
-                              convex = TRUE)
-  }
-  # setting candidate learners if not specified
-  if (is.null(learners) & outcome_type %in% c("binomial", "quasibinomial", "continuous")) {
-    learners <- sl3::make_learner_stack(sl3::Lrnr_glm, sl3::Lrnr_mean)
-  } else if (!is.null(learners)) {
-    learners <- learners
-  }
-
-  # returns
-  out <- list(learners = learners,
-              metalearner = meta)
-
-  return(out)
-}
-
 run_ensemble <- function(ensemble, task, envir) {
   if (!is.null(ensemble)) {
     ensemble$train(task)
@@ -83,7 +59,7 @@ predict_sl3 <- function(object, task, envir) {
     call_data <- eval(substitute(task$data), envir = envir)
     call_obj  <- substitute(object)
     out       <- eval(call("predict.glm", call_obj, newdata = call_data, type = "response"),
-                envir = envir)
+                      envir = envir)
     return(as.vector(out))
   }
 }
