e63dc43ddf771b46e5cc765a64ac7fca714a75c0
Nick Williams <ntwilliams.personal@gmail.com>
2022-03-04 13:08:17 -0800

    Code style improvements.

--- tests/testthat/test-point_treatment.R
+++ tests/testthat/test-point_treatment.R
@@ -1,4 +1,3 @@
-
 context("Fidelity of estimators for a point treatment")
 
 set.seed(543523)
@@ -14,13 +13,10 @@ Y <- Y.1 * A + Y.0 * (1 - A)
 tmp <- data.frame(W1, W2, A, Y, Y.1, Y.0)
 truth <- mean(tmp$Y.1)
 
-sub <- lmtp_sub(tmp, "A", "Y", baseline = c("W1", "W2"), shift = static_binary_on, folds = 2)
-
-ipw <- lmtp_ipw(tmp, "A", "Y", baseline = c("W1", "W2"), shift = static_binary_on, folds = 2)
-
-tmle <- lmtp_tmle(tmp, "A", "Y", baseline = c("W1", "W2"), shift = static_binary_on, folds = 2)
-
-sdr <- lmtp_sdr(tmp, "A", "Y", baseline = c("W1", "W2"), shift = static_binary_on, folds = 2)
+sub <- lmtp_sub(tmp, "A", "Y", baseline = c("W1", "W2"), shift = static_binary_on, folds = 1)
+ipw <- lmtp_ipw(tmp, "A", "Y", baseline = c("W1", "W2"), shift = static_binary_on, folds = 1)
+tmle <- lmtp_tmle(tmp, "A", "Y", baseline = c("W1", "W2"), shift = static_binary_on, folds = 1)
+sdr <- lmtp_sdr(tmp, "A", "Y", baseline = c("W1", "W2"), shift = static_binary_on, folds = 1)
 
 # tests
 test_that("point treatment fidelity", {
