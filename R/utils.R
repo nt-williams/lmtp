91ff2e9d538465d8bfc1418cf41ed0a1fad45127
Nick Williams <ntwilliams.personal@gmail.com>
2022-03-10 09:03:13 -0800

    File structure changes. Removing cross-fit.R, renaming prepare-estimators.R

--- R/utils.R
+++ R/utils.R
@@ -13,6 +13,23 @@ set_lmtp_options <- function(option, val) {
   )
 }
 
+setup_cv <- function(data, id, V = 10) {
+  out <- origami::make_folds(data, cluster_ids = id, V = V)
+  if (V > 1) {
+    return(out)
+  }
+  out[[1]]$training_set <- out[[1]]$validation_set
+  out
+}
+
+get_folded_data <- function(data, folds, index) {
+  out <- list()
+  out[["train"]] <- data[folds[[index]]$training_set, , drop = FALSE]
+  out[["valid"]] <- data[folds[[index]]$validation_set, , drop = FALSE]
+  out
+}
+
+
 bound <- function(x, p = getOption("lmtp.bound")) {
   pmax(pmin(x, 1 - p), p)
 }
