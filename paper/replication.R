d595d8f664dbf2407067c5f47ec2f653f12e18b1
Nick Williams <ntwilliams.personal@gmail.com>
2022-04-12 14:12:11 -0700

    draft of paper complete.

--- paper/replication.R
+++ paper/replication.R
@@ -42,62 +42,54 @@ lmtp_sdr(sim_cens, A, "Y", time_vary = L, cens = C,
          shift = f, intervention_type = "mtp", folds = 1,
          learners_trt = sl_lib, learners_outcome = sl_lib)
 
+lmtp_sdr(sim_cens, A, "Y", time_vary = L, cens = C,
+         shift = NULL, folds = 1, learners_trt = sl_lib, learners_outcome = sl_lib)
+
 # Example 3 ---------------------------------------------------------------
 
-x <- "trt"
-w <- c("W1", "W2")
-cen <- paste0("C.", 0:5)
-y <- paste0("Y.", 1:6)
+A <- "trt"
+W <- c("W1", "W2")
+C <- paste0("C.", 0:5)
+Y <- paste0("Y.", 1:6)
 
 set.seed(5423)
-tml1 <- lmtp_tmle(sim_point_surv, x, y, w, cens = cen,
-                  learners_trt = lrnrs, learners_outcome = lrnrs,
-                  shift = static_binary_on, folds = 3)
+a_1 <- lmtp_tmle(sim_point_surv, A, Y, W, cens = C,
+                 outcome_type = "survival", folds = 1,
+                 learners_trt = sl_lib,
+                 learners_outcome = sl_lib,
+                 shift = static_binary_on)
 
-set.seed(6354)
-tml0 <- lmtp_tmle(sim_point_surv, x, y, w, cens = cen,
-                  learners_trt = lrnrs, learners_outcome = lrnrs,
-                  shift = static_binary_off, folds = 3)
+a_0 <- lmtp_tmle(sim_point_surv, A, Y, W, cens = C,
+                 outcome_type = "survival", folds = 1,
+                 learners_trt = sl_lib,
+                 learners_outcome = sl_lib,
+                 shift = static_binary_off)
 
-lmtp_contrast(tml1, ref = tml0, type = "rr")
-
-set.seed(432)
-sdr1 <- lmtp_sdr(sim_point_surv, x, y, w, cens = cen,
-                 learners_trt = lrnrs, learners_outcome = lrnrs,
-                 shift = static_binary_on, folds = 3)
-
-set.seed(143)
-sdr0 <- lmtp_sdr(sim_point_surv, x, y, w, cens = cen,
-                 learners_trt = lrnrs, learners_outcome = lrnrs,
-                 shift = static_binary_off, folds = 3)
-
-lmtp_contrast(sdr1, ref = sdr0, type = "rr")
+lmtp_contrast(a_1, ref = a_0, type = "rr")
 
 # Example 4 ---------------------------------------------------------------
 
-x <- c("A_1", "A_2", "A_3", "A_4")
-tv <- list(c("L_1"), c("L_2"), c("L_3"), c("L_4"))
-y <- "Y"
+A <- c("A_1", "A_2", "A_3", "A_4")
+L <- list(c("L_1"), c("L_2"), c("L_3"), c("L_4"))
 
-shift <- function(data, trt) {
-  (data[[trt]] - 1) * (data[[trt]] - 1 >= 1) +
-    data[[trt]] * (data[[trt]] - 1 < 1)
-}
-
-dynamic_shift <- function(data, trt) {
-  if (trt == "A_1") {
-    shift(data, trt)
-  } else {
-    ifelse(data[[sub("A", "L", trt)]] == 1,
-           shift(data, trt),
-           data[[trt]])
+f <- function(data, trt) {
+  mtp <- function(data, trt) {
+    (data[[trt]] - 1) * (data[[trt]] - 1 >= 1) +
+      data[[trt]] * (data[[trt]] - 1 < 1)
   }
-}
 
-set.seed(540)
-lmtp_tmle(sim_t4, x, y, time_vary = tv, shift = dynamic_shift, folds = 3,
-          learners_outcome = lrnrs, learners_trt = lrnrs)
+  if (trt == "A_1") {
+    return(mtp(data, trt))
+  }
+
+  ifelse(
+    data[[sub("A", "L", trt)]] == 1,
+    mtp(data, trt),
+    data[[trt]]
+  )
+}
 
 set.seed(697)
-lmtp_sdr(sim_t4, x, y, time_vary = tv, shift = dynamic_shift, folds = 3,
-         learners_outcome = lrnrs, learners_trt = lrnrs)
+lmtp_sdr(sim_t4, A, "Y", time_vary = L, shift = f,
+         intervention_type = "mtp", folds = 1,
+         learners_outcome = sl_lib, learners_trt = sl_lib)
