102002bfa1beb9290ba9e9a34c2a24ffadd16014
nt-williams <ntwilliams.personal@gmail.com>
2024-06-29 16:31:59 -0700

    Refactoring for loop in lmtp_survival

--- R/lmtp_survival.R
+++ R/lmtp_survival.R
@@ -67,7 +67,8 @@ lmtp_survival <- function(data, trt, outcomes, baseline = NULL, time_vary = NULL
                           id = NULL,
                           learners_outcome = "SL.glm",
                           learners_trt = "SL.glm",
-                          folds = 10, weights = NULL,
+                          folds = 10,
+                          weights = NULL,
                           control = lmtp_control()) {
 
   checkmate::assertCharacter(outcomes, min.len = 2, null.ok = FALSE, unique = TRUE, any.missing = FALSE)
@@ -76,34 +77,34 @@ lmtp_survival <- function(data, trt, outcomes, baseline = NULL, time_vary = NULL
   tau <- length(outcomes)
   estimates <- vector("list", tau)
 
+  args <- list(
+    data = data,
+    trt = trt,
+    baseline = baseline,
+    time_vary = time_vary,
+    shift = shift,
+    shifted = shifted,
+    k = k,
+    mtp = mtp,
+    id = id,
+    learners_outcome = learners_outcome,
+    learners_trt = learners_trt,
+    folds = folds,
+    weights = weights,
+    control = control
+  )
+
+  if (estimator == "lmtp_tmle") expr <- expression(do.call(lmtp_tmle, args))
+  else expr <- expression(do.call(lmtp_sdr, args))
+
   t <- 1
   cli::cli_progress_step("Working on time {t}/{tau}...")
   for (t in 1:tau) {
-    args <- list(
-      data = data,
-      trt = trt,
-      outcome = outcomes[1:t],
-      baseline = baseline,
-      time_vary = time_vary,
-      cens = cens[1:t],
-      shift = shift,
-      shifted = shifted,
-      k = k,
-      mtp = mtp,
-      outcome_type = ifelse(t == 1, "binomial", "survival"),
-      id = id,
-      learners_outcome = learners_outcome,
-      learners_trt = learners_trt,
-      folds = folds,
-      weights = weights,
-      control = control
-    )
+    args$outcome <- outcomes[1:t]
+    args$cens <- cens[1:t]
+    args$outcome_type <- ifelse(t == 1, "binomial", "survival")
 
-    estimates[[t]] <- future::future({
-      if (estimator == "lmtp_tmle") do.call(lmtp_tmle, args)
-      else do.call(lmtp_sdr, args)
-    },
-    seed = TRUE)
+    estimates[[t]] <- future::future(eval(expr), seed = TRUE)
     cli::cli_progress_update()
   }
 
@@ -122,7 +123,7 @@ isotonic_projection <- function(x, alpha = 0.05) {
   iso_fit <- isotone::gpava(1:length(x), estim$estimate)
   for (i in seq_along(x)) {
     x[[i]]$theta <- iso_fit$y[i]
-    x[[i]]$low  <- x[[i]]$theta - (qnorm(0.975) * x[[i]]$standard_error)
+    x[[i]]$low <- x[[i]]$theta - (qnorm(0.975) * x[[i]]$standard_error)
     x[[i]]$high <- x[[i]]$theta + (qnorm(0.975) * x[[i]]$standard_error)
   }
   x
