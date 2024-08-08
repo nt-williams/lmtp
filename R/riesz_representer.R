3de1c654a28c9b782c8fad2d4b495eebb3f11b70
Herb Susmann <herbps10@gmail.com>
2024-08-08 16:27:42 -0400

    Update riesz estimation to work with new SuperRiesz update

--- R/riesz_representer.R
+++ R/riesz_representer.R
@@ -30,6 +30,7 @@ estimate_rr <- function(natural, shifted, G, trt, cens, risk, tau, conditional, 
   fits <- list()
 
   prev_riesz <- matrix(1, ncol = 1, nrow = nrow(natural$train))
+  prev_riesz_valid <- matrix(1, ncol = 1, nrow = nrow(natural$valid))
 
   for (t in 1:tau) {
     jrt <- censored(natural$train, cens, t)$j
@@ -48,26 +49,31 @@ estimate_rr <- function(natural, shifted, G, trt, cens, risk, tau, conditional, 
 
     vars <- c(node_list[[t]], cens[[t]])
 
-    cumulative_indicator_train <- matrix(as.logical(apply(conditional$train[, (t + 1):(tau + 1), drop = FALSE], 1, prod)), ncol = 1)
-    cumulative_indicator_valid <- matrix(as.logical(apply(conditional$valid[, (t + 1):(tau + 1), drop = FALSE], 1, prod)), ncol = 1)
+    conditional_t_train <- conditional$train[, t]
+    conditional_t_valid <- conditional$valid[, t]
+
+    cumulative_indicator_train <- matrix(as.logical(apply(conditional$train[, (t):(tau + 1), drop = FALSE], 1, prod)), ncol = 1)
+    cumulative_indicator_valid <- matrix(as.logical(apply(conditional$valid[, (t):(tau + 1), drop = FALSE], 1, prod)), ncol = 1)
 
     new_shifted <- natural$train
-    new_shifted[[trt_t]] <- shifted$train[[trt_t]]
+    new_shifted[, trt_t] <- shifted$train[, trt_t]
 
     new_shifted_valid <- natural$valid
-    new_shifted_valid[[trt_t]] <- shifted$valid[[trt_t]]
+    new_shifted_valid[, trt_t] <- shifted$valid[, trt_t]
 
     fit <- run_riesz_ensemble(
       learners,
       natural$train[jrt & drt, vars, drop = FALSE],
       new_shifted[jrt & drt, vars, drop = FALSE],
       cumulative_indicator_train[jrt & drt, drop = FALSE],
-      G$train[jrt & drt, t + 1, drop = FALSE],
+      G$train[jrt & drt, t, drop = FALSE],
       natural$valid[jrv & drv, vars, drop = FALSE],
       new_shifted_valid[jrv & drv, vars, drop = FALSE],
       cumulative_indicator_valid[jrv & drv, drop = FALSE],
-      G$valid[jrv & drv, t + 1, drop = FALSE],
+      G$valid[jrv & drv, t, drop = FALSE],
+      conditional_t_valid,
       prev_riesz,
+      prev_riesz_valid,
       folds = control$.learners_trt_folds
     )
 
@@ -80,6 +86,7 @@ estimate_rr <- function(natural, shifted, G, trt, cens, risk, tau, conditional, 
     pred <- matrix(-999L, nrow = nrow(natural$valid), ncol = 1)
     pred[jrv & drv, ] <- fit$predictions
     prev_riesz <- matrix(fit$predictions_train, ncol = 1)
+    prev_riesz_valid <- matrix(fit$predictions, ncol = 1)
 
     representers[, t] <- pred
 
