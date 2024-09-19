387070a59126c499bc0efae4284c599de6805947
Nicholas Williams <ntwilliams.personal@gmail.com>
2024-09-19 09:18:47 -0700

    Accounting for ignored boot call in lmtp_survival

--- R/lmtp_survival.R
+++ R/lmtp_survival.R
@@ -100,8 +100,12 @@ lmtp_survival <- function(data, trt, outcomes, baseline = NULL, time_vary = NULL
   if (length(trt) == 1) args$trt <- trt
   if (length(time_vary) == 1) args$time_vary <- time_vary
 
-  if (estimator == "lmtp_tmle") expr <- expression(do.call(lmtp_tmle, args))
-  else expr <- expression(do.call(lmtp_sdr, args))
+  if (estimator == "lmtp_tmle") {
+    args$boot <- boot
+    expr <- expression(do.call(lmtp_tmle, args))
+  } else {
+    expr <- expression(do.call(lmtp_sdr, args))
+  }
 
   t <- 1
   cli::cli_progress_step("Working on time {t}/{tau}...")
