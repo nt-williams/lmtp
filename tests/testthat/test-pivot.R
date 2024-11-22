46f3d95312e1b9d6bef5ed61174f87254023417f
Nicholas Williams <ntwilliams.personal@gmail.com>
2024-11-22 09:46:31 -0800

    Update pivot tests

--- tests/testthat/test-pivot.R
+++ tests/testthat/test-pivot.R
@@ -11,7 +11,7 @@ test_that("pivot wide to long correctly", {
     tmp$..i..lmtp_id <- tmp$ID
     tmp$ID <- NULL
 
-    head(pivot(tmp, foo, 4))
+    head(pivot(tmp, foo))
 
     foo <- LmtpVars$new(NULL,
                         list(c("L1"), c("L2")),
@@ -22,14 +22,6 @@ test_that("pivot wide to long correctly", {
     tmp <- sim_cens
     tmp$..i..lmtp_id <- 1:nrow(tmp)
 
-    head(pivot(tmp, foo, 2))
-
-    foo <- LmtpVars$new(c("W1", "W2"), NULL, "trt", paste0("C.", 0:5), paste0("Y.", 1:6), 6)
-
-    task <- LmtpTask$new(sim_point_surv, shifted, "trt",
-                         paste0("Y.", 1:6), NULL, c("W1", "W2"),
-                         paste0("C.", 0:5), Inf, "id", "survival", 1, NULL)
-
-    head(pivot(task$natural, task$vars, task$tau), 12)
+    head(pivot(tmp, foo))
   })
 })
