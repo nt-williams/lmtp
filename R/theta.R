df3b4f73f180245ed21bcdbac0a3a1432bb7aa04
Nicholas Williams <ntwilliams.personal@gmail.com>
2025-03-05 11:15:17 -0800

    Remove curve algo for now

--- R/theta.R
+++ R/theta.R
@@ -24,36 +24,3 @@ theta_dr <- function(task, m, r, fits_m, fits_r, shift, augmented = FALSE) {
   class(out) <- "lmtp"
   out
 }
-
-theta_curve <- function(task, m, r, fits_m, fits_r, shift) {
-  ics <- lapply(1:task$tau, function(t) {
-    eif(r[, 1:t, drop = FALSE], m$shifted[[t]], m$natural[[t]])
-  })
-
-  thetas <- unlist(lapply(ics, \(x) weighted.mean(x, task$weights)))
-
-  ics <- lapply(ics, \(x) task$rescale(x))
-  thetas <- task$rescale(thetas)
-
-  if (task$survival) {
-    iso_projection <- isotone::gpava(seq_along(thetas), 1 - thetas)
-    thetas <- 1 - iso_projection$x[seq_along(thetas)]
-  }
-
-  out <- list(
-    estimator = "SDR curve",
-    estimates = lapply(
-      seq_along(ics),
-      \(x) ife::ife(thetas[[x]], ics[[x]], task$weights, as.character(task$id))
-    ),
-    outcome_reg = lapply(m$shifted, \(x) task$rescale(x)),
-    density_ratios = r,
-    fits_m = fits_m,
-    fits_r = fits_r,
-    shift = shift,
-    outcome_type = task$outcome_type
-  )
-
-  class(out) <- "lmtp_curve"
-  out
-}
