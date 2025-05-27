e54e19381a3dcfad4083f8cff55cc2a16f6f1f64
Nicholas Williams <ntwilliams.personal@gmail.com>
2025-05-27 15:24:47 -0400

    basic params for flip

--- R/flip_sdr.R
+++ R/flip_sdr.R
@@ -1,3 +1,16 @@
-flip_sdr <- function() {
+flip_sdr <- function(data, trt, outcome, baseline = NULL,
+                     time_vary = NULL,
+                     cens = NULL, compete = NULL,
+                     overlap = FALSE, # if FALSE, use smooth trimming, else use overlap weights
+                     trimming_threshold, #epsilon
+                     smoothing_constant, # k
+                     k = Inf,
+                     outcome_type = c("binomial", "continuous", "survival"),
+                     id = NULL, bounds = NULL,
+                     learners_outcome = "SL.glm",
+                     learners_trt = "SL.glm",
+                     folds = 10, weights = NULL,
+                     control = lmtp_control(),
+) {
 
 }
