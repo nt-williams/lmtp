a0e937a69ebc0ed88545b50bb21b067d51ed79b5
Nicholas Williams <ntwilliams.personal@gmail.com>
2025-08-22 09:57:43 -0700

    Fitting propensity score for abritrary number of levels

--- R/propensity_score.R
+++ R/propensity_score.R
@@ -10,19 +10,18 @@ cf_propensity_score <- function(task, learners, control, pb) {
 
   ans <- future::value(ans)
 
+  propensity_scores <- lapply(seq_len(task$time_horizon), function(time) {
+    this_time <- lapply(seq_along(task$folds), function(fold) {
+      ans[[fold]]$propensity_score[, , time]
+    })
+    recombine(Reduce(rbind, this_time), task$folds)
+  })
+
   ans <- list(
-    propensity_score = lapply(seq_len(task$time_horizon), function(time) {
-      this_time <- lapply(seq_along(task$folds), function(fold) {
-        ans[[fold]]$propensity_score[[time]]
-      })
-      recombine(Reduce(rbind, this_time), task$folds)
-    }),
-    prob_observed = recombine(
-      rbind_depth(ans, "prob_observed"),
-      task$folds
-    ),
-    fits_trt = lapply(ans, function(x) x[["fits_trt"]]),
-    fits_cens = lapply(ans, function(x) x[["fits_cens"]])
+    propensity_score = simplify2array(propensity_scores),
+    prob_observed = recombine(rbind_depth(ans, "probability_observed"), task$folds),
+    learner_treatment_summary = rbind_depth(ans, "learner_treatment_summary"),
+    learner_cens_summary = rbind_depth(ans, "learner_cens_summary")
   )
 
   # TODO: trimming
@@ -32,66 +31,69 @@ cf_propensity_score <- function(task, learners, control, pb) {
 
 estimate_propensity_score <- function(task, fold, learners, control, pb) {
   data <- get_folded_data(task$natural, task$folds, fold)
+  levels <- task$support(task$time_horizon)
+  number_levels <- length(levels)
 
-  # Create list of matrices the same number as \tau to store predictions
-  propensity_scores <- lapply(seq_len(task$time_horizon),
-                              \(time) matrix(nrow = nrow(data$valid), ncol = 2))
-  prob_observed <- matrix(1, nrow = nrow(data$valid), ncol = task$time_horizon)
-  fits_trt <- vector("list", length = task$time_horizon)
-  fits_cens <- fits_trt
+  # Create arrays to store predictions
+  propensity_scores <- array(NA_real_, c(nrow(data$train), number_levels, task$time_horizon), dimnames = list(NULL, levels, NULL))
+  probability_observed <- matrix(1, nrow = nrow(data$valid), ncol = task$time_horizon)
+
+  learner_treatment_summary <- NULL
+  learner_cens_summary <- learner_treatment_summary
 
   for (time in seq_len(task$time_horizon)) {
     # Get indices of observations that aren't censored and haven't experienced
     # the outcome or the competing risk
-    i <- task$observed(data$train, time - 1) %and%
-      task$is_at_risk(data$train, time)
+    i <- task$observed(data$train, time - 1) %and% task$is_at_risk(data$train, time)
+    iv <- task$observed(data$valid, time - 1) %and% task$is_at_risk(data$valid, time)
 
     # Current treatment variable
-    A_t <- current_trt(task$vars$A, time)
+    this_treatment <- current_trt(task$vars$A, time)
     # Current censoring variable
-    C_t <- task$vars$C[time]
+    this_censoring <- task$vars$C[time]
 
     # Covariates
     vars <- c("..i..lmtp_id", task$vars$history("A", time))
 
-    # Need to fit K-1 models for K levels
-    # Just assuming binary for now
-    fit_trt <- run_ensemble(
-      data$train[i, c(vars, A_t)], A_t,
-      learners,
-      "binomial", "..i..lmtp_id",
-      control$.learners_trt_folds
-    )
+    # Treatment levels at this time
+    levels <- task$support(task$time_horizon)
+    number_levels <- length(levels)
 
-    if (!is.null(C_t)) {
-      fit_cens <- run_ensemble(
-        data[i, c(vars, A_t, C_t)], C_t,
-        learners,
-        "binomial", "..i..lmtp_id",
-        control$.learners_trt_folds
+    # One hot encode the treatment
+    ohe <- model.matrix(~ -1 + as.character(data$train[[this_treatment]]))
+    colnames(ohe) <- gsub("as.character\\(data\\$train\\[\\[this_treatment\\]\\]\\)", "", colnames(ohe))
+
+    # Loop over K-1 treatment levels as binomial models
+    for (l in 2:number_levels) {
+      this_level <- colnames(ohe)[l]
+      tmp <- data$train
+      tmp[[this_treatment]] <- ohe[, l]
+      fit <- run_ensemble(
+        tmp[i, c(vars, this_treatment)], this_treatment,
+        learners, "binomial", "..i..lmtp_id", control$.learners_trt_folds
       )
-    } else {
-      fit_cens <- NULL
+
+      propensity_scores[iv, this_level, time] <- predict(fit, data$valid[iv, ])
+
+      # Add fit summary
+      learner_treatment_summary <- rbind(learner_treatment_summary, summary(fit, time, fold, level = this_level))
     }
 
-    if (control$.return_full_fits) {
-      fits_trt[[time]] <- fit_trt
-      fits_cens[[time]] <- fit_cens
-    } else {
-      fits_trt[[time]] <- extract_sl_weights(fit_trt)
-      fits_cens[[time]] <- extract_sl_weights(fit_cens)
+    # Add probability for the reference level
+    propensity_scores[iv, colnames(ohe)[1], time] <- 1 - rowSums(propensity_scores[iv, colnames(ohe)[2:number_levels], time, drop = FALSE])
+
+    # Fit model for censoring if there is censoring
+    if (!is.null(this_censoring)) {
+      fit <- run_ensemble(
+        data[i, c(vars, this_treatment, this_censoring)], this_censoring,
+        learners, "binomial", "..i..lmtp_id", control$.learners_trt_folds
+      )
+
+      # Add fit summary
+      learner_cens_summary <- rbind(learner_cens_summary, summary(fit, time, fold, level = NA_character_))
     }
 
-    i <- task$observed(data$valid, time - 1) %and%
-      task$is_at_risk(data$valid, time)
-
-    # j=2 will index A = 1, j=1 will index A = 0
-    # TODO: dont hard code this
-    propensity_scores[[time]][i, 2] <- predict(fit_trt, data$valid[i, ])
-    propensity_scores[[time]][i, 1] <- 1 - propensity_scores[[time]][i, 2]
-    colnames(propensity_scores[[time]]) <- c("0", "1")
-
-    if (!is.null(C_t)) {
+    if (!is.null(this_censoring)) {
       # TODO: predict probability of being observed
     }
 
@@ -99,7 +101,7 @@ estimate_propensity_score <- function(task, fold, learners, control, pb) {
   }
 
   list(propensity_score = propensity_scores,
-       prob_observed = prob_observed,
-       fits_trt = fits_trt,
-       fits_cens = fits_cens)
+       probability_observed = probability_observed,
+       learner_treatment_summary = learner_treatment_summary,
+       learner_cens_summary = learner_cens_summary)
 }
