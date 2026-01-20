7af639098b814b642df1df514707cfaae19451d7
Nicholas Williams <ntwilliams.personal@gmail.com>
2026-01-20 15:07:54 -0800

    Theta function for ltmle

--- R/theta.R
+++ R/theta.R
@@ -24,3 +24,32 @@ theta_dr <- function(task, sequential_regressions, density_ratios, fits_m, fits_
   class(out) <- "lmtp"
   out
 }
+
+theta_ltmle <- function(task, estimates, propensity_scores, levels) {
+  theta <- sapply(estimates, function(x) fmean(x$predictions[, 1], w = task$weights))
+
+  # Rescale estimates
+  theta <- sapply(theta, function(x) task$rescale(x))
+  influence_functions <- lapply(estimates, function(x) task$rescale(x$uncentered_eif))
+
+  # Create 'ife' objects
+  ifes <- lapply(seq_along(theta), function(i) {
+    ife::ife(theta[i], influence_functions[[i]], task$weights, as.character(task$id))
+  })
+  names(estimates) <- levels
+
+  out <- list(
+    estimator = "TMLE",
+    estimates = ifes,
+    outcome_reg = lapply(estimates, function(x) task$rescale(x$predictions)),
+    propensity_scores = propensity_scores$propensity_score,
+    prob_observed = propensity_scores$prob_observed,
+    fits_outcome = lapply(estimates, function(x) x$learner_outcome_summary),
+    fits_treatment = propensity_scores$learner_treatment_summary,
+    fits_censoring = propensity_scores$learner_cens_summary,
+    outcome_type = task$outcome_type
+  )
+
+  class(out) <- "ltmle"
+  out
+}
