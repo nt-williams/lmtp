6777a8120fe0c8955768e0c276ee6ce3392f626f
Nicholas Williams <ntwilliams.personal@gmail.com>
2026-01-20 15:09:35 -0800

    Function to sequentially construct eif

--- R/eif.R
+++ R/eif.R
@@ -10,3 +10,11 @@ compute_weights <- function(density_ratios, time, time_horizon) {
   if (ncol(out) > ncol(density_ratios)) return(t(out))
   out
 }
+
+update_tmle_eif <- function(eif, Y, riesz, predictions) {
+  if (missing(eif)) eif <- numeric(length(Y))
+  # Multiply Riesz representer by residual
+  component <- riesz * (Y - predictions)
+  # Add to the given EIF
+  eif + component
+}
