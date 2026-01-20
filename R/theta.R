fff58e8aea037876bd3ebce2327f118f602aa52d
Nicholas Williams <ntwilliams.personal@gmail.com>
2026-01-20 15:16:30 -0800

    Refactor theta_dr as theta_lmtp

--- R/theta.R
+++ R/theta.R
@@ -1,23 +1,21 @@
-theta_dr <- function(task, sequential_regressions, density_ratios, fits_m, fits_r, shift, is_sdr) {
-  influence_function <- eif(density_ratios, sequential_regressions$shifted, sequential_regressions$natural)
-
+theta_lmtp <- function(task, estimates, density_ratios, shift, is_sdr) {
   if (is_sdr) {
-    theta <- weighted.mean(influence_function, task$weights)
+    theta <- fmean(influence_function, w = task$weights)
   } else {
-    theta <- weighted.mean(sequential_regressions$shifted[, 1], task$weights)
+    theta <- fmean(estimates$predictions[, 1], w = task$weights)
   }
 
-  influence_function <- task$rescale(influence_function)
+  influence_function <- task$rescale(estimates$uncentered_eif)
   theta <- task$rescale(theta)
 
   out <- list(
     estimator = ifelse(is_sdr, "SDR", "TMLE"),
     estimate = ife::ife(theta, influence_function, task$weights, as.character(task$id)),
     shift = shift,
-    outcome_reg = task$rescale(sequential_regressions$shifted),
-    density_ratios = density_ratios,
-    fits_m = fits_m,
-    fits_r = fits_r,
+    outcome_reg = task$rescale(estimates$predictions),
+    density_ratios = density_ratios$density_ratios,
+    fits_outcome = estimates$learner_outcome_summary,
+    fits_treatment = density_ratios$fits,
     outcome_type = task$outcome_type
   )
 
