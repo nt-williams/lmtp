0170eff1c210dca4ebc20bb143526bd1ad856b01
Nicholas Williams <ntwilliams.personal@gmail.com>
2026-06-09 14:08:12 -0700

    Use fmean and refactor theta.R

--- R/theta.R
+++ R/theta.R
@@ -1,10 +1,10 @@
-theta_dr <- function(task, sequential_regressions, density_ratios, fits_m, fits_r, shift, is_sdr) {
+theta_lmtp <- function(task, sequential_regressions, density_ratios, fits_m, fits_r, shift, is_sdr) {
   influence_function <- eif(density_ratios, sequential_regressions$shifted, sequential_regressions$natural)
 
   if (is_sdr) {
-    theta <- weighted.mean(influence_function, task$weights)
+    theta <- fmean(influence_function, w = task$weights)
   } else {
-    theta <- weighted.mean(sequential_regressions$shifted[, 1], task$weights)
+    theta <- fmean(sequential_regressions$shifted[, 1], w = task$weights)
   }
 
   influence_function <- task$rescale(influence_function)
@@ -12,12 +12,12 @@ theta_dr <- function(task, sequential_regressions, density_ratios, fits_m, fits_
 
   out <- list(
     estimator = ifelse(is_sdr, "SDR", "TMLE"),
-    estimate = ife::ife(theta, influence_function, task$weights, as.character(task$id)),
+    estimate = ife(theta, influence_function, task$weights, as.character(task$id)),
     shift = shift,
     outcome_reg = task$rescale(sequential_regressions$shifted),
     density_ratios = density_ratios,
-    fits_m = fits_m,
-    fits_r = fits_r,
+    fits_outcome = fits_m,
+    fits_treatment = fits_r,
     outcome_type = task$outcome_type
   )
 
