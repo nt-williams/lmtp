9b4f1bbadc8453952b7cd894a7ca465a7743a4c4
Nicholas Williams <ntwilliams.personal@gmail.com>
2026-06-11 11:27:25 -0700

    Update survey weight test with ltmle

--- tests/testthat/test-survey.R
+++ tests/testthat/test-survey.R
@@ -6,14 +6,14 @@ n <- 1e4
 W1 <- rbinom(n, size = 1, prob = 0.5)
 W2 <- rbinom(n, size = 1, prob = 0.65)
 A <- rbinom(n, size = 1, prob = plogis(-0.4 + 0.2 * W2 + 0.15 * W1))
-Y.1 <- rbinom(n, size = 1, prob = plogis(-1 + 1 - 0.1 * W1 + 0.3 * W2))
-Y.0 <- rbinom(n, size = 1, prob = plogis(-1 + 0 - 0.1 * W1 + 0.3 * W2))
+Y.1 <- rbinom(n, size = 1, prob = plogis(-1 + 1 + 1.5 * W1 + 0.3 * W2))
+Y.0 <- rbinom(n, size = 1, prob = plogis(-1 + 0 + 1.5 * W1 + 0.3 * W2))
 
 Y <- Y.1 * A + Y.0 * (1 - A)
 tmp <- data.frame(W1, W2, A, Y, Y.1, Y.0)
 truth <- mean(tmp$Y.1)
 
-prob_S <- plogis(W1 * 0.5 + rnorm(n, mean = 0, sd = 1))
+prob_S <- plogis(2 * W1 - 1)
 S <- rbinom(n, 1, prob_S)
 tmp <- tmp[S == 1, ]
 wts <- 1 / prob_S[S == 1]
@@ -24,8 +24,11 @@ tmle <- sw(lmtp_tmle(tmp, "A", "Y", baseline = c("W1", "W2"), shift = static_bin
 sdr <- sw(lmtp_sdr(tmp, "A", "Y", baseline = c("W1", "W2"), shift = static_binary_on,
                 weights = wts, folds = 2))
 
+tmle2 <- sw(ltmle(tmp, "A", "Y", baseline = c("W1", "W2"), weights = wts, folds = 2))
+
 # tests
 test_that("survey weight fidelity", {
   expect_equal(truth, tmle$estimate@x, tolerance = 0.01)
   expect_equal(truth, sdr$estimate@x, tolerance = 0.01)
+  expect_equal(truth, as.vector(tmle2$estimates$`1`@x), tolerance = 0.01)
 })
